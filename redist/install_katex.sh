9ff6536b4dad9484700afa44a902b8f1b2fa30fd
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-08-17 21:01:14 +0200

    fix katex fonts/images install

--- redist/install_katex.sh
+++ redist/install_katex.sh
@@ -4,3 +4,5 @@ curl -L 'https://github.com/Khan/KaTeX/releases/download/v0.8.1/katex.tar.gz' > 
 tar -xzvf katex.tar.gz
 cp -r ./katex/fonts ./katex/images ./katex/katex.css ../styles
 rm katex.tar.gz
+sed -i -e "s/url('fonts/url('atom:\/\/ink\/styles\/fonts/g" ../styles/katex.css
+sed -i -e "s/url(images/url(atom:\/\/ink\/styles\/images/g" ../styles/katex.css
