7acc3997eec70b9b4a8525d4ca608d0d20db15af
Sebastian Pfitzner <pfitzseb@physik.hu-berlin.de>
2017-12-18 15:55:37 +0100

    whoops

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -19,7 +19,8 @@ function installKaTeX () {
 }
 
 function installXTerm () {
-
+  fs.copySync(__dirname + '/../node_modules/xterm/lib/xterm.css', __dirname + '/../styles/xterm.css')
 }
 
 installKaTeX()
+installXTerm()
